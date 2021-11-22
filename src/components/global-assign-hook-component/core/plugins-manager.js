3f56ac381f8444b4e29ab514e2d7cfc76b1b99a1
kkoooqq <>
2021-11-21 21:32:49 -0500

    fix: relative path error

--- src/components/global-assign-hook-component/core/plugins-manager.js
+++ src/components/global-assign-hook-component/core/plugins-manager.js
@@ -1,15 +1,16 @@
-const fs = require("fs");
+const fs = require('fs');
+const path = require('path');
 
 // 管理插件加载，打包注入到页面中
 
 const pluginsNames = [
-    "string-put-to-db-plugins.js",
-    "search-strings-db-plugins.js",
-    "eval-hook-plugins.js",
+    'string-put-to-db-plugins.js',
+    'search-strings-db-plugins.js',
+    'eval-hook-plugins.js',
 ];
 
 let lastReadDiskTs = 0;
-let pluginsJsCodeCache = "";
+let pluginsJsCodeCache = '';
 
 // 带缓存的读取插件，是为了在支持热加载的情况下尽可能的少读硬盘
 function loadPluginsAsStringWithCache() {
@@ -26,36 +27,36 @@ function loadPluginsAsString() {
 
     // 用来保证Hook代码只被加载一次
     // TODO 妥善处理Worker环境
-    const loadOnce = "\n" +
-        "   if (!window) {\n" +
-        "       return; \n" +
-        "   } \n" +
-        "   if (window.cc11001100_hook_done) {\n" +
-        "       return;\n" +
-        "   }\n" +
-        "   window.cc11001100_hook_done = true;\n\n";
+    const loadOnce = '\n' +
+        '   if (!window) {\n' +
+        '       return; \n' +
+        '   } \n' +
+        '   if (window.cc11001100_hook_done) {\n' +
+        '       return;\n' +
+        '   }\n' +
+        '   window.cc11001100_hook_done = true;\n\n';
 
-    const hookJsCode = fs.readFileSync("../components/global-assign-hook-component/core/hook.js").toString();
+    const hookJsCode = fs.readFileSync(path.resolve(__dirname, './hook.js')).toString();
 
     const pluginsJsContentArray = [];
-    const pluginsBaseDirectory = "../components/global-assign-hook-component/plugins/";
+    const pluginsBaseDirectory = '../components/global-assign-hook-component/plugins/';
     for (let pluginName of pluginsNames) {
-        const pluginFilePath = pluginsBaseDirectory + "/" + pluginName;
+        const pluginFilePath = pluginsBaseDirectory + '/' + pluginName;
         const pluginJsContent = fs.readFileSync(pluginFilePath).toString();
         pluginsJsContentArray.push(pluginJsContent);
     }
 
-    return "\n// ----------------------------------------- Hook代码开始 ----------------------------------------------------- \n" +
-        "\n(() => {\n" +
+    return '\n// ----------------------------------------- Hook代码开始 ----------------------------------------------------- \n' +
+        '\n(() => {\n' +
 
         loadOnce +
 
         hookJsCode +
 
-        pluginsJsContentArray.join("\n\n") +
+        pluginsJsContentArray.join('\n\n') +
 
-        "})();\n" +
-        "\n// ----------------------------------------- Hook代码结束 ----------------------------------------------------- \n\n\n\n\n";
+        '})();\n' +
+        '\n// ----------------------------------------- Hook代码结束 ----------------------------------------------------- \n\n\n\n\n';
 }
 
 
